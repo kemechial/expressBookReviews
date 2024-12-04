591994872c96f6b35fd7bc0a5c10c799e836be3f
kemechial <kaanevran@gmail.com>
2024-12-04 04:46:20 -0500

    Task 5: find book review by isbn, find book by title.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,14 +48,29 @@ public_users.get('/author/:author',function (req, res) {
     });
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title; 
+    matched_books = [];
+      Object.values(books).forEach(book =>{
+        if (book.title == title){
+             matched_books.push(book);
+        }
+      });
+      if(matched_books.length > 0){
+        res.status(200).send(
+            JSON.stringify(matched_books, null, 4)
+        );
+      } else {
+        res.send("The title was not found!");  
+      }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+       // Extract the lastName parameter from the request URL
+       const isbn = req.params.isbn;
+       // Filter the users array to find users whose lastName matches the extracted lastName parameter
+       let filtered_book = books[isbn];
+       res.send(filtered_book['reviews']);
 });
 
 module.exports.general = public_users;
