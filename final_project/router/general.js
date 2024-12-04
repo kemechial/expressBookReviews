362d64372a55c2ef484e3aee1e085972217c90e2
kemechial <kaanevran@gmail.com>
2024-12-04 03:38:33 -0500

    Task 2: retrieve book detail based on isbn.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,18 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+     // Extract the lastName parameter from the request URL
+    const isbn = req.params.isbn;
+    // Filter the users array to find users whose lastName matches the extracted lastName parameter
+    let filtered_book = books[isbn];
+    // Send the filtered_lastname array as the response to the client
+    res.send(filtered_book);
  });
   
 // Get book details based on author
